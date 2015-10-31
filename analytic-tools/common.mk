7b7c402fc1b10d256cd900198a69c3d0f7e4d95d
Eddie Antonio Santos <easantos@ualberta.ca>
2015-10-31 15:39:00 -0600

    Create terrifying make-build system.

--- analytic-tools/common.mk
+++ analytic-tools/common.mk
@@ -1,6 +1,3 @@
-# Get absolute path to containing directory: http://stackoverflow.com/a/324782
-TOP := $(dir $(CURDIR)/$(word $(words $(MAKEFILE_LIST)),$(MAKEFILE_LIST)))
-LOCAL_INCLUDE_DIR := $(TOP)Modules
-
+# Compilation flags for all files. 
 CFLAGS = -ansi
-CPPFLAGS = -MMD -I$(LOCAL_INCLUDE_DIR)
+CPPFLAGS = -I$(LOCAL_INCLUDE_DIR)
