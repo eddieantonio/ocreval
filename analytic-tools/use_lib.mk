2a5b5805c34e069ab2cea016c57d05432ca92368
Eddie Antonio Santos <easantos@ualberta.ca>
2015-10-31 18:24:32 -0600

    Link with libraries in the right order.

--- analytic-tools/use_lib.mk
+++ analytic-tools/use_lib.mk
@@ -1,7 +1,11 @@
 # Get absolute path to containing directory: http://stackoverflow.com/a/324782
 TOP := $(dir $(CURDIR)/$(word $(words $(MAKEFILE_LIST)),$(MAKEFILE_LIST)))
-LOCAL_INCLUDE_DIR := $(TOP)Modules
+LOCAL_INCLUDE_DIR := $(abspath $(TOP)Modules)
 
 include $(TOP)/common.mk
 
-LDFLAGS = $(LOCAL_INCLUDE_DIR)/Lib.a
+LDFLAGS = $(LOCAL_INCLUDE_DIR)/Lib.a -lm
+
+# ld(1) needs the LDFLAGS at the end for some dumb reason.
+%: %.c
+	$(CC) $(CFLAGS) $(CPPFLAGS) -o $@ $< $(LDFLAGS)
