b06b1ebe6f4245258725deb409130120af26d4ee
Eddie Antonio Santos <easantos@ualberta.ca>
2015-11-26 11:20:37 -0700

    Specify as X/Open 5.0 source.
    This removes warnings on GCC/glibc about strdup() being undefined, or
    returning an int.

--- common.mk
+++ common.mk
@@ -1,6 +1,9 @@
 # Compilation flags for all files.
 CFLAGS += -ansi
+# This code was originally written in the mid-90s;
+# X/Open 5.0 was all there was :/
+CPPDEFINES = -D_XOPEN_SOURCE=500
 # utf8proc lib usually lives in here:
-CPPFLAGS += -I/usr/local/include
+CPPFLAGS := $(CPPFLAGS) -I/usr/local/include $(CPPDEFINES)
 LDFLAGS += -L/usr/local/lib
 LDLIBS = -lm -lutf8proc
