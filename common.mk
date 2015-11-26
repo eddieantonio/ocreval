c4c7102a13e4fbf6afd2bb139658d87b5dde7a4e
Eddie Antonio Santos <easantos@ualberta.ca>
2015-11-26 12:35:28 -0700

    Use -D_XOPEN_SOURCE=600 to play nice on OS X.

--- common.mk
+++ common.mk
@@ -1,8 +1,7 @@
 # Compilation flags for all files.
 CFLAGS += -ansi
-# This code was originally written in the mid-90s;
-# X/Open 5.0 was all there was :/
-CPPDEFINES = -D_XOPEN_SOURCE=500
+# X/Open 6.0 standardizes features used in this K&R C source...
+CPPDEFINES = -D_XOPEN_SOURCE=600
 # utf8proc lib usually lives in here:
 CPPFLAGS := $(CPPFLAGS) -I/usr/local/include $(CPPDEFINES)
 LDFLAGS += -L/usr/local/lib
