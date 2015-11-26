5fe690b54ae3653ee73ab5c3c48166f6c60906e8
Eddie Antonio Santos <easantos@ualberta.ca>
2015-11-26 09:00:46 -0700

    Re-style text.{c,h}.

--- Modules/text.c
+++ Modules/text.c
@@ -3,7 +3,7 @@
  *  text.c
  *
  *  Author: Stephen V. Rice
- *  
+ *
  * Copyright 1996 The Board of Regents of the Nevada System of Higher
  * Education, on behalf, of the University of Nevada, Las Vegas,
  * Information Science Research Institute
@@ -32,98 +32,98 @@
 
 /*
  * Assert that the internal character size is the same size as UTF-32.
- * (C89 style!)
+ * (C89 static assert.)
  */
 static int assert_sizes[sizeof(Charvalue) == sizeof(utf8proc_int32_t) ? 0 : -1];
 
 /**********************************************************************/
 
 void append_char(text, suspect, value)
-Text *text;
-Boolean suspect;
-Charvalue value;
+	Text *text;
+	Boolean suspect;
+	Charvalue value;
 {
-    Char *c;
-    c = NEW(Char);
-    c->suspect = suspect;
-    c->value = value;
-    list_insert_last(text, c);
+	Char *c;
+	c = NEW(Char);
+	c->suspect = suspect;
+	c->value = value;
+	list_insert_last(text, c);
 }
 /**********************************************************************/
 
 static Boolean read_header(f)
-FILE *f;
+	FILE *f;
 {
-    char line[100];
-    short divider_count = 0;
-    if (fgets(line, sizeof(line) - 1, f) &&
-    strncmp(line, TITLE, sizeof(TITLE) - 5) == 0)
-    {
-	while (divider_count < 2 && fgets(line, sizeof(line) - 1, f))
-	    if (strcmp(line, DIVIDER) == 0)
-		divider_count++;
-	return(True);
-    }
-    rewind(f);
-    return(False);
+	char line[100];
+	short divider_count = 0;
+	if (fgets(line, sizeof(line) - 1, f) &&
+			strncmp(line, TITLE, sizeof(TITLE) - 5) == 0)
+	{
+		while (divider_count < 2 && fgets(line, sizeof(line) - 1, f))
+			if (strcmp(line, DIVIDER) == 0)
+				divider_count++;
+		return(True);
+	}
+	rewind(f);
+	return(False);
 }
 /**********************************************************************/
 
 static void read_contents(f, text, find_markers, suspect_marker)
-FILE *f;
-Text *text;
-Boolean find_markers;
-int suspect_marker;
+	FILE *f;
+	Text *text;
+	Boolean find_markers;
+	int suspect_marker;
 {
-    Boolean suspect = False;
+	Boolean suspect = False;
 
-    /* Buffer for one full UTF-8 code unit. */
-    utf8proc_uint8_t buffer[4];
+	/* Buffer for one full UTF-8 code unit. */
+	utf8proc_uint8_t buffer[4];
 
-    int byte, read_status, decode_status;
-    ssize_t code_unit_size;
-    utf8proc_int32_t code_point;
-    byte = getc(f);
-
-    while (byte != EOF)
-    {
-	code_unit_size = utf8proc_utf8class[byte];
-	if (code_unit_size < 1) {
-	    /* Encoding error! Stream is not UTF-8! */
-	    error("invalid UTF-8 character", Exit);
-	}
-
-	buffer[0] = byte;
-	/* Read the remaining bytes into the buffer. */
-	if (code_unit_size > 1) {
-	    read_status = fread(
-		    buffer + 1,
-		    sizeof(utf8proc_uint8_t),
-		    /* The first byte is already in the buffer. */
-		    code_unit_size - 1,
-		    f
-	    );
-
-	    if (read_status != (code_unit_size - 1)) {
-		/* Could not read enough bytes. */
-		error("unexpected end of input", Exit);
-	    }
-	}
-
-	/* Decode a single code unit -> code point. */
-	decode_status = utf8proc_iterate(buffer, sizeof(buffer), &code_point);
-	if (decode_status != code_unit_size) {
-	    error(utf8proc_errmsg(decode_status), Exit);
-	}
-
-	if (find_markers && code_point == suspect_marker) {
-	    suspect = True;
-	} else {
-	    append_char(text, suspect, code_point);
-	    suspect = False;
-	}
+	int byte, read_status, decode_status;
+	ssize_t code_unit_size;
+	utf8proc_int32_t code_point;
 	byte = getc(f);
-    }
+
+	while (byte != EOF)
+	{
+		code_unit_size = utf8proc_utf8class[byte];
+		if (code_unit_size < 1) {
+			/* Encoding error! Stream is not UTF-8! */
+			error("invalid UTF-8 character", Exit);
+		}
+
+		buffer[0] = byte;
+		/* Read the remaining bytes into the buffer. */
+		if (code_unit_size > 1) {
+			read_status = fread(
+					buffer + 1,
+					sizeof(utf8proc_uint8_t),
+					/* The first byte is already in the buffer. */
+					code_unit_size - 1,
+					f
+					);
+
+			if (read_status != (code_unit_size - 1)) {
+				/* Could not read enough bytes. */
+				error("unexpected end of input", Exit);
+			}
+		}
+
+		/* Decode a single code unit -> code point. */
+		decode_status = utf8proc_iterate(buffer, sizeof(buffer), &code_point);
+		if (decode_status != code_unit_size) {
+			error(utf8proc_errmsg(decode_status), Exit);
+		}
+
+		if (find_markers && code_point == suspect_marker) {
+			suspect = True;
+		} else {
+			append_char(text, suspect, code_point);
+			suspect = False;
+		}
+		byte = getc(f);
+	}
 }
 /**********************************************************************/
 
@@ -131,115 +131,115 @@ int suspect_marker;
  * Blank characters EXCEPT the newline!
  */
 static Boolean is_blank(character)
-utf8proc_int32_t character;
+	utf8proc_int32_t character;
 {
-    const utf8proc_property_t * props = utf8proc_get_property(character);
+	const utf8proc_property_t * props = utf8proc_get_property(character);
 
-    /* Cases from ISRI < 6.0 */
-    switch (character) {
-	case NEWLINE:
-	    /* Special case: a newline is a NEWLINE; not a BLANK. */
-	    return False;
+	/* Cases from ISRI < 6.0 */
+	switch (character) {
+		case NEWLINE:
+			/* Special case: a newline is a NEWLINE; not a BLANK. */
+			return False;
 
-	case '\t':
-	case LINE_TABULATION:
-	case FORM_FEED:
-	case CARRIAGE_RETURN:
-	case NON_BREAKING_SPACE:
-	    return True;
-    }
+		case '\t':
+		case LINE_TABULATION:
+		case FORM_FEED:
+		case CARRIAGE_RETURN:
+		case NON_BREAKING_SPACE:
+			return True;
+	}
 
-    /* See: http://www.unicode.org/versions/Unicode8.0.0/ch04.pdf */
-    switch (props->category) {
-	case UTF8PROC_CATEGORY_ZS:  /* Space characters. */
-	case UTF8PROC_CATEGORY_ZL:  /* Line separators. */
-	case UTF8PROC_CATEGORY_ZP:  /* Paragraph separators. */
-	    return True;
-    }
+	/* See: http://www.unicode.org/versions/Unicode8.0.0/ch04.pdf */
+	switch (props->category) {
+		case UTF8PROC_CATEGORY_ZS:  /* Space characters. */
+		case UTF8PROC_CATEGORY_ZL:  /* Line separators. */
+		case UTF8PROC_CATEGORY_ZP:  /* Paragraph separators. */
+			return True;
+	}
 
-    return False;
+	return False;
 }
 
 
 /**********************************************************************/
 
 static void compress_spacing(text, start)
-Text *text;
-Char *start;
+	Text *text;
+	Char *start;
 {
-    Boolean found_non_blank = False;
-    Char *c, *next;
+	Boolean found_non_blank = False;
+	Char *c, *next;
 
-    /* Normalize all blank-y charcaters to BLANK. */
-    for (c = start; c; c = c->next)
-	if (is_blank(c->value))
-	    c->value = BLANK;
+	/* Normalize all blank-y charcaters to BLANK. */
+	for (c = start; c; c = c->next)
+		if (is_blank(c->value))
+			c->value = BLANK;
 
-    c = start;
-    while (c)
-    {
-	next = c->next;
-	if ((c->value == BLANK && (!found_non_blank || !next ||
-		next->value == BLANK || next->value == NEWLINE)) ||
-	    (c->value == NEWLINE && !found_non_blank))
+	c = start;
+	while (c)
 	{
-	    if (found_non_blank && next && next->value == BLANK)
-		next->suspect |= c->suspect;
-	    list_remove(text, c);
-	    free(c);
-	} else {
-	    found_non_blank = (c->value == NEWLINE ? False : True);
-	}
+		next = c->next;
+		if ((c->value == BLANK && (!found_non_blank || !next ||
+						next->value == BLANK || next->value == NEWLINE)) ||
+				(c->value == NEWLINE && !found_non_blank))
+		{
+			if (found_non_blank && next && next->value == BLANK)
+				next->suspect |= c->suspect;
+			list_remove(text, c);
+			free(c);
+		} else {
+			found_non_blank = (c->value == NEWLINE ? False : True);
+		}
 
-	c = next;
-    }
+		c = next;
+	}
 }
 /**********************************************************************/
 
 static void lowercase(start)
-Char *start;
+	Char *start;
 {
-    Char *c;
-    for (c = start; c; c = c->next)
-	/* Delegate to utf8proc. */
-	c->value = utf8proc_tolower(c->value);
+	Char *c;
+	for (c = start; c; c = c->next)
+		/* Delegate to utf8proc. */
+		c->value = utf8proc_tolower(c->value);
 }
 
 /**********************************************************************/
 
 void read_text(text, filename, textopt)
-Text *text;
-char *filename;
-Textopt *textopt;
+	Text *text;
+	char *filename;
+	Textopt *textopt;
 {
-    Char *last;
-    FILE *f;
-    if (textopt->find_header && !filename)
-	error_string("invalid call to", "read_text", Exit);
-    last = text->last;
-    f = open_file(filename, "r");
-    if (textopt->find_header)
-	textopt->found_header = read_header(f);
-    read_contents(f, text, textopt->find_markers, 
-    (textopt->suspect_marker ? textopt->suspect_marker : SUSPECT_MARKER));
-    close_file(f);
-    if (textopt->normalize)
-	compress_spacing(text, (last ? last->next : text->first));
-    if (textopt->case_insensitive)
-	lowercase(last ? last->next : text->first);
+	Char *last;
+	FILE *f;
+	if (textopt->find_header && !filename)
+		error_string("invalid call to", "read_text", Exit);
+	last = text->last;
+	f = open_file(filename, "r");
+	if (textopt->find_header)
+		textopt->found_header = read_header(f);
+	read_contents(f, text, textopt->find_markers,
+			(textopt->suspect_marker ? textopt->suspect_marker : SUSPECT_MARKER));
+	close_file(f);
+	if (textopt->normalize)
+		compress_spacing(text, (last ? last->next : text->first));
+	if (textopt->case_insensitive)
+		lowercase(last ? last->next : text->first);
 }
 
 /**********************************************************************/
 signed char encode_or_die(codepoint, buffer)
-utf8proc_int32_t codepoint;
-utf8proc_uint8_t *buffer;
+	utf8proc_int32_t codepoint;
+	utf8proc_uint8_t *buffer;
 {
-    utf8proc_ssize_t status = utf8proc_encode_char(codepoint, buffer);
-    if (status < 1) {
-	error("could not encode UTF-8", Exit);
-    }
+	utf8proc_ssize_t status = utf8proc_encode_char(codepoint, buffer);
+	if (status < 1) {
+		error("could not encode UTF-8", Exit);
+	}
 
-    return status;
+	return status;
 }
 /**********************************************************************/
 
@@ -247,91 +247,91 @@ utf8proc_uint8_t *buffer;
  * Is this generally considered to be a graphic character?
  */
 static Boolean is_graphic_character(character)
-utf8proc_int32_t character;
+	utf8proc_int32_t character;
 {
-    const utf8proc_property_t * props = utf8proc_get_property(character);
+	const utf8proc_property_t * props = utf8proc_get_property(character);
 
-    switch (props->category) {
-	case UTF8PROC_CATEGORY_LU: /**< Letter, uppercase */
-	case UTF8PROC_CATEGORY_LL: /**< Letter, lowercase */
-	case UTF8PROC_CATEGORY_LT: /**< Letter, titlecase */
-	case UTF8PROC_CATEGORY_LM: /**< Letter, modifier */
-	case UTF8PROC_CATEGORY_LO: /**< Letter, other */
-	case UTF8PROC_CATEGORY_MN: /**< Mark, nonspacing */
-	case UTF8PROC_CATEGORY_MC: /**< Mark, spacing combining */
-	case UTF8PROC_CATEGORY_ME: /**< Mark, enclosing */
-	case UTF8PROC_CATEGORY_ND: /**< Number, decimal digit */
-	case UTF8PROC_CATEGORY_NL: /**< Number, letter */
-	case UTF8PROC_CATEGORY_NO: /**< Number, other */
-	case UTF8PROC_CATEGORY_PC: /**< Punctuation, connector */
-	case UTF8PROC_CATEGORY_PD: /**< Punctuation, dash */
-	case UTF8PROC_CATEGORY_PS: /**< Punctuation, open */
-	case UTF8PROC_CATEGORY_PE: /**< Punctuation, close */
-	case UTF8PROC_CATEGORY_PI: /**< Punctuation, initial quote */
-	case UTF8PROC_CATEGORY_PF: /**< Punctuation, final quote */
-	case UTF8PROC_CATEGORY_PO: /**< Punctuation, other */
-	case UTF8PROC_CATEGORY_SM: /**< Symbol, math */
-	case UTF8PROC_CATEGORY_SC: /**< Symbol, currency */
-	case UTF8PROC_CATEGORY_SK: /**< Symbol, modifier */
-	case UTF8PROC_CATEGORY_SO: /**< Symbol, other */
-	/* Allow for private use characters. */
-	case UTF8PROC_CATEGORY_CO: /**< Other, private use */
-	    return True;
-	default:
-	    return False;
-    }
+	switch (props->category) {
+		case UTF8PROC_CATEGORY_LU: /**< Letter, uppercase */
+		case UTF8PROC_CATEGORY_LL: /**< Letter, lowercase */
+		case UTF8PROC_CATEGORY_LT: /**< Letter, titlecase */
+		case UTF8PROC_CATEGORY_LM: /**< Letter, modifier */
+		case UTF8PROC_CATEGORY_LO: /**< Letter, other */
+		case UTF8PROC_CATEGORY_MN: /**< Mark, nonspacing */
+		case UTF8PROC_CATEGORY_MC: /**< Mark, spacing combining */
+		case UTF8PROC_CATEGORY_ME: /**< Mark, enclosing */
+		case UTF8PROC_CATEGORY_ND: /**< Number, decimal digit */
+		case UTF8PROC_CATEGORY_NL: /**< Number, letter */
+		case UTF8PROC_CATEGORY_NO: /**< Number, other */
+		case UTF8PROC_CATEGORY_PC: /**< Punctuation, connector */
+		case UTF8PROC_CATEGORY_PD: /**< Punctuation, dash */
+		case UTF8PROC_CATEGORY_PS: /**< Punctuation, open */
+		case UTF8PROC_CATEGORY_PE: /**< Punctuation, close */
+		case UTF8PROC_CATEGORY_PI: /**< Punctuation, initial quote */
+		case UTF8PROC_CATEGORY_PF: /**< Punctuation, final quote */
+		case UTF8PROC_CATEGORY_PO: /**< Punctuation, other */
+		case UTF8PROC_CATEGORY_SM: /**< Symbol, math */
+		case UTF8PROC_CATEGORY_SC: /**< Symbol, currency */
+		case UTF8PROC_CATEGORY_SK: /**< Symbol, modifier */
+		case UTF8PROC_CATEGORY_SO: /**< Symbol, other */
+			/* Allow for private use characters. */
+		case UTF8PROC_CATEGORY_CO: /**< Other, private use */
+			return True;
+		default:
+			return False;
+	}
 }
 
 /**********************************************************************/
 
 void char_to_string(suspect, value, string, fake_newline)
-Boolean suspect;
-Charvalue value;
-char *string;
-Boolean fake_newline;
+	Boolean suspect;
+	Charvalue value;
+	char *string;
+	Boolean fake_newline;
 {
-    short i = 0;
-    utf8proc_uint8_t *ustring = (utf8proc_uint8_t *) string;
+	short i = 0;
+	utf8proc_uint8_t *ustring = (utf8proc_uint8_t *) string;
 
-    /* Prepend the suspect marker. */
-    if (suspect) {
-	i += encode_or_die(SUSPECT_MARKER, ustring);
-    }
+	/* Prepend the suspect marker. */
+	if (suspect) {
+		i += encode_or_die(SUSPECT_MARKER, ustring);
+	}
 
-    if (value == NEWLINE) {
-	sprintf(&string[i], (fake_newline ? "<\\n>" : "\n"));
-    } else if (is_graphic_character(value)) {
-	/* It's a printable character. */
-	i += encode_or_die(value, &ustring[i]);
-	string[i++] = '\0';
-    } else if (value < 256) {
-	sprintf(&string[i], "<%02X>", value);
-    } else {
-	sprintf(&string[i], "<%04X>", value);
-    }
+	if (value == NEWLINE) {
+		sprintf(&string[i], (fake_newline ? "<\\n>" : "\n"));
+	} else if (is_graphic_character(value)) {
+		/* It's a printable character. */
+		i += encode_or_die(value, &ustring[i]);
+		string[i++] = '\0';
+	} else if (value < 256) {
+		sprintf(&string[i], "<%02X>", value);
+	} else {
+		sprintf(&string[i], "<%04X>", value);
+	}
 }
 /**********************************************************************/
 
 void write_text(text, filename, write_header)
-Text *text;
-char *filename;
-void (*write_header)();
+	Text *text;
+	char *filename;
+	void (*write_header)();
 {
-    FILE *f;
-    Char *c;
-    char string[STRING_SIZE];
-    f = open_file(filename, "w");
-    if (write_header)
-    {
-	fputs(TITLE, f);
-	fputs(DIVIDER, f);
-	(*write_header)(f);
-	fputs(DIVIDER, f);
-    }
-    for (c = text->first; c; c = c->next)
-    {
-	char_to_string(c->suspect, c->value, string, False);
-	fputs(string, f);
-    }
-    close_file(f);
+	FILE *f;
+	Char *c;
+	char string[STRING_SIZE];
+	f = open_file(filename, "w");
+	if (write_header)
+	{
+		fputs(TITLE, f);
+		fputs(DIVIDER, f);
+		(*write_header)(f);
+		fputs(DIVIDER, f);
+	}
+	for (c = text->first; c; c = c->next)
+	{
+		char_to_string(c->suspect, c->value, string, False);
+		fputs(string, f);
+	}
+	close_file(f);
 }
--- Modules/text.h
+++ Modules/text.h
@@ -10,7 +10,7 @@
  *
  *  Author: Stephen V. Rice (1996)
  *  Author: Eddie Antonio Santos (2015)
- *  
+ *
  * Copyright 1996 The Board of Regents of the Nevada System of Higher
  * Education, on behalf, of the University of Nevada, Las Vegas,
  * Information Science Research Institute
@@ -37,18 +37,25 @@
 #include "list.h"
 #include "util.h"
 
-#define BLANK		    ' '
-#define NEWLINE		    '\n'
+#define BLANK               ' '
+#define NEWLINE             '\n'
 #define LINE_TABULATION     0x00B
 #define FORM_FEED           0x00C
 #define CARRIAGE_RETURN     0x00D
 #define NON_BREAKING_SPACE  0x0A0
 #define REJECT_CHARACTER    '~'
-#define SUSPECT_MARKER	    '^'
+#define SUSPECT_MARKER      '^'
 
-/* UTF-32. */
+/* Use UTF-32 internally. */
 typedef uint32_t Charvalue;
+/* Technically, there are far less *scalar values* (what you and I call
+ * "characters") in Unicode, but using the max number of *code points* here
+ * makes the implementation simpler. See: http://www.unicode.org/glossary/ */
 #define NUM_CHARVALUES  0x10FFFF
+
+/* Maxiumum char size of a single char_to_string() operation, including the
+ * null-terminator. */
+#define STRING_SIZE (max(sizeof("^🦄"), sizeof("<10FFFF>")))
 
 BEGIN_ITEM(Char)
     Boolean suspect;
@@ -59,67 +66,61 @@ BEGIN_LIST_OF(Char)
 END_LIST(Text);
 
 void append_char(/* Text *text, Boolean suspect, Charvalue value */);
-			/* appends the given character to "text" */
+                        /* appends the given character to "text" */
 
 typedef
 struct
 {
     Boolean find_header;/* if True, a header will be looked for and skipped if
-			   present; if False and a header is present, it will be
-			   stored as text */
+                           present; if False and a header is present, it will be
+                           stored as text */
     Boolean find_markers;
-			/* if True, any occurrence of the "suspect_marker"
-			   character will be interpreted as marking the
-			   following character as suspect */
+                        /* if True, any occurrence of the "suspect_marker"
+                           character will be interpreted as marking the
+                           following character as suspect */
 
-    Charvalue suspect_marker; /* applicable when "find_markers" is True; if zero,
-			   SUSPECT_MARKER will be used */
-    /* Deprecated! Silently ignored. */
+    Charvalue suspect_marker;
+                        /* applicable when "find_markers" is True; if zero,
+                           SUSPECT_MARKER will be used */
     Boolean find_hex_values;
-			/* if True, any occurrence of a hex value of the form
-			   <FF> or <FFFF> will be converted to the corresponding
-			   character value; if False, any such string will be
-			   stored as text */
-    Boolean normalize;	/* if True, spacing is compressed */
+                        /* deprecated and silently ignored */
+    Boolean normalize;  /* if True, spacing is compressed */
     Boolean case_insensitive;
-			/* if True, letters are converted to lower-case */
+                        /* if True, letters are converted to lower-case */
     Boolean found_header;
-			/* set to True if a header was found; applicable when
-			   "find_header" is True */
-    /* TODO: add NFC and NFD options. */
+                        /* set to True if a header was found; applicable when
+                           "find_header" is True */
 } Textopt;
 
 void read_text(/* Text *text, char *filename, Textopt *textopt */);
-			/* reads the named file (or stdin if "filename" is NULL
-			   and "textopt->find_header" is False) based on the
-			   options specified in "textopt", and appends each
-			   character to "text"; reports an error and quits if
-			   unable to open the file */
-
-#define STRING_SIZE (max(sizeof("^🦄"), sizeof("<10FFFF>")))
+                        /* reads the named file (or stdin if "filename" is NULL
+                           and "textopt->find_header" is False) based on the
+                           options specified in "textopt", and appends each
+                           character to "text"; reports an error and quits if
+                           unable to open the file */
 
 void char_to_string(/* Boolean suspect, Charvalue value, char *string,
-		       Boolean fake_newline */);
-			/* stores a representation of the given character in
-			   "string", which must be at least STRING_SIZE bytes; a
-			   non-printable character is represented by a hex value
-			   of the form <FF> or <FFFF>; if "fake_newline" is
-			   True, the newline character is represented by <\n>,
-			   which is desirable for some reports */
+                       Boolean fake_newline */);
+                        /* stores a representation of the given character in
+                           "string", which must be at least STRING_SIZE bytes; a
+                           non-printable character is represented by a hex value
+                           of the form <FF> or <FFFF>; if "fake_newline" is
+                           True, the newline character is represented by <\n>,
+                           which is desirable for some reports */
 
 signed char encode_or_die(/* Charvalue value, char *string */);
-			/* writes a Unicode value to the given string;
-			 * exits if the value cannot be written;
-			 * returns characters written to string */
+                        /* writes a Unicode value to the given string;
+                         * exits if the value cannot be written;
+                         * returns characters written to string */
 
 
 void write_text(/* Text *text, char *filename,
-		   void (*write_header)(FILE *f) */);
-			/* writes each character of "text" to the named file
-			   (or stdout if "filename" is NULL) using
-			   "char_to_string" to represent the characters;
-			   if "write_header" is non-NULL, this routine is
-			   called to write a header to the file; reports an
-			   error and quits if unable to create the file */
+                   void (*write_header)(FILE *f) */);
+                        /* writes each character of "text" to the named file
+                           (or stdout if "filename" is NULL) using
+                           "char_to_string" to represent the characters;
+                           if "write_header" is non-NULL, this routine is
+                           called to write a header to the file; reports an
+                           error and quits if unable to create the file */
 
 #endif
