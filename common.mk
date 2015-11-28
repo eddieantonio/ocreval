2416b8e550cfa59c324c3d8e9f1007fb3e29d212
Eddie Antonio Santos <easantos@ualberta.ca>
2015-11-28 13:11:01 -0700

    Allow extension of CFLAGS on cmdline.

--- common.mk
+++ common.mk
@@ -1,5 +1,5 @@
 # Compilation flags for all files.
-CFLAGS += -ansi
+override CFLAGS := $(CFLAGS) -ansi
 # X/Open 6.0 standardizes features used in this K&R C source...
 CPPDEFINES = -D_XOPEN_SOURCE=600
 # utf8proc lib usually lives in here:
