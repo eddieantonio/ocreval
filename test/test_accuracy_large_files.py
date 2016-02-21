7bd8a5fe92678bf00b002fede36344e74a2770c6
Eddie Antonio Santos <easantos@ualberta.ca>
2016-02-21 01:47:36 -0700

    Update test to not rely on Unix words file.

--- test/test_accuracy_large_files.py
+++ test/test_accuracy_large_files.py
@@ -1,30 +1,53 @@
 #!/usr/bin/env python
 # -*- encoding: UTF-8 -*-
 
-from __future__ import print_function
-
 """
-Tests accuracy on LARGE files.
+Tests accuracy on LARGE UTF-8 files.
 """
 
 import random
-import string
 import subprocess
 import tempfile
 
 import os.path as p
 
 
+# Alias range as xrange in Python 3:
 try:
     xrange
 except NameError:
     xrange = range
 
+# Create a Python 2/3 Unicode string literal:
+try:
+    unicode
+except NameError:
+    u = str
+else:
+    u = lambda s: s.decode('UTF-8')
 
 # Path to accuracy program
 ACCURACY = p.join(p.dirname(p.dirname(p.realpath(__file__))),
-                        'bin', 'accuracy')
+                  'bin', 'accuracy')
 assert p.exists(ACCURACY), 'Could not find ' + ACCURACY
+
+
+# http://www.languagegeek.com/isolate/haidastory.html
+corpus = u('''\
+Aaniisuu tangaa g̱aging.ang ’wan suuga. ’Ll xidgwangaas, x̱uyaa’aa. Tllgu
+ḵ’aawgyaa hllng.aaygi ’laa ḵyaang.aas. Ḵawdiuu gwaay g̱ud gwaa nang ḵadlaa
+ḵ’ayg̱udyaas ’laagu ḵ’aawgaay g̱an ’laa g̱á ’laa xidaas. Á tl’l sg̱aana ḵidaads
+’yaahlgaagaas g̱iinuus gangaang ’laagu gud gwii x̱iihlt’ahliyaagaas. Ga
+sg̱aanag̱waa g̱ax̱aas ’laa t’isda ḵ’a sḵ’agilaang.aas, tll gwii x̱an, hahl gwii’ad
+wah gwii’aa. G̱adagaas gyaanuu’asing g̱aalgaagaang ’wan suuga.
+
+Nang kilsdlaas naag̱ag̱a.aw tadll chi’a’aawaagan. Sing ḵ’alg̱ada ’ll ḵaaxuhls
+gyaan ’ll kindagaang.aas. Sda ’laa xid k’udahldats’aasii gyaan gagu ’laa
+ḵ’aw’aawaasgu x̱an ’laa ḵ’aawgangas.
+''')
+
+dictionary = tuple(word for word in corpus.split())
+alphabet = [char for char in corpus if char not in ' \n']
 
 
 def one_in(n):
@@ -33,7 +56,7 @@ def one_in(n):
 
 def change_letter(word):
     letter_index = random.choice(xrange(len(word)))
-    mutation = random.choice(string.ascii_lowercase)
+    mutation = random.choice(alphabet)
     return word[:letter_index] + mutation + word[letter_index + 1:]
 
 
@@ -41,28 +64,30 @@ if __name__ == '__main__':
     import sys
     amount_of_words = int(sys.argv[1]) if len(sys.argv) > 1 else 32768
 
-    # Load up a bunch of real words.
-    with open('/usr/share/dict/words', 'r') as word_file:
-        dictionary = [line.strip().lower() for line in word_file]
-
     # Create temporary files for each...
-    with tempfile.NamedTemporaryFile('w') as correct_file,\
-            tempfile.NamedTemporaryFile('w') as generated_file:
+    with tempfile.NamedTemporaryFile('wb') as correct_file,\
+            tempfile.NamedTemporaryFile('wb') as generated_file:
 
         # Generate A LOT of random words
         for _ in xrange(amount_of_words):
-            end = '\n' if one_in(10) else ' '
+            end = b'\n' if one_in(10) else b' '
 
             word = random.choice(dictionary)
-            print(word, end=end, file=correct_file)
+            correct_file.write(word.encode('UTF-8'))
 
             # Occasionally, typo a word in the generated file.
             generated_word = change_letter(word) if one_in(1000) else word
-            print(generated_word, end=end, file=generated_file)
+            generated_file.write(generated_word.encode('UTF-8'))
+
+            # Write a space or newline.
+            correct_file.write(end)
+            generated_file.write(end)
 
         # Finish off the file with a new line and flush the output.
-        print(file=correct_file)
-        print(file=generated_file)
+        if end != b'\n':
+            correct_file.write(b'\n')
+            generated_file.write(b'\n')
+
         correct_file.flush()
         generated_file.flush()
 
